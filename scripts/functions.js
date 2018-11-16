c796075d32e60e5cd8b173dba009a5d5963465a1
Dustin <Dustinrmb@gmail.com>
2018-11-16 11:55:33 -0500

    22222

---
+++ scripts/functions.js
@@ -0,0 +1,3 @@
+function warning(){
+    alert("Greetings visitor. This site was made as a school assignment. Most news was cited from varius creditable news sources such as CNN. Please  go to those sites instead of this one if your intent is to gather credible news information from a trusted and well known source. Thank you for your cooperation")
+}
